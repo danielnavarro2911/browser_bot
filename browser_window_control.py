b30d7892235ffdec161ead21d1a9192f9833fffd
Daniel Navarro Quiros <96166574+danielnavarro2911@users.noreply.github.com>
2024-10-16 11:15:29 -0600

    Update browser_window_control.py

--- browser_window_control.py
+++ browser_window_control.py
@@ -1,4 +1,10 @@
-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class Browser:
     def __init__(self, URL='', incognito=False, headless=False, maximized=True):
